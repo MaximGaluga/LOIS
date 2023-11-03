f252acce3264bed541f1782b1e02e7e5e30032d7
MaximGaluga <91080628+MaximGaluga@users.noreply.github.com>
2023-11-03 21:00:00 +0300

    added support for writing implications

--- LR1/main.py
+++ LR1/main.py
@@ -1,3 +1,5 @@
 from utils.ParseFile import ParseFile
 
 parsed_data = ParseFile.parse_file('./input.txt')
+
+print(parsed_data)
--- LR1/utils/ParseFile.py
+++ LR1/utils/ParseFile.py
@@ -2,28 +2,37 @@ import re
 
 
 class ParseFile:
-    __PATTERN = r'([A-Z]+=\{\([a-z]\d,\d+\.\d+\)(?:,\([a-z]\d,\d+\.\d+\))*)\}'
-    __CASH = {}
+    __SETS_PATTERN = r'^[A-Z]+=\{\([a-z]\d,\d+\.\d+\)(?:,\([a-z]\d,\d+\.\d+\))*\}$'
+    __IMPLICATIONS_PATTERN = r'^[A-Z]->[A-Z]$'
+    __CASH = {
+        "sets": {},
+        "implications": []
+    }
 
     @staticmethod
     def parse_file(url: str):
         txt = ParseFile.__open_file(url).replace(' ', '')
-        text = re.findall(ParseFile.__PATTERN, txt)
+        sets = re.findall(ParseFile.__SETS_PATTERN, txt, re.MULTILINE)
 
-        for current_set in text:
+        for current_set in sets:
             find_set_name = r'[A-Z]+'
             set_name = re.findall(find_set_name, current_set)
-            ParseFile.__CASH[set_name[0]] = set()
+            ParseFile.__CASH["sets"][set_name[0]] = list()
 
             find_dataset = r'\([a-z]\d,\d+\.\d+\)'
             dataset = re.findall(find_dataset, current_set)
 
             for data in dataset:
-                value = data.replace('(', '').replace(')', '')
-                value = value.split(',')
-                ParseFile.__CASH[set_name[0]].add((value[0], float(value[1])))
+                values = data.replace('(', '').replace(')', '')
+                values = values.split(',')
+                ParseFile.__CASH["sets"][set_name[0]].append((values[0], float(values[1])))
 
-        print(ParseFile.__CASH)
+        implications = re.findall(ParseFile.__IMPLICATIONS_PATTERN, txt, re.MULTILINE)
+        for implication in implications:
+            values = implication.split('->')
+            ParseFile.__CASH["implications"].append((values[0], values[1]))
+
+        return ParseFile.__CASH
 
     @staticmethod
     def __open_file(url: str) -> str:
